c51e8d167738b258925e5349d88c9232ea091169
Talia Samantha <taliasamanthar@gmail.com>
2025-11-15 23:37:40 +0100

    change

--- angularApp/src/app/app.config.ts
+++ angularApp/src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 } from '@angular/core';
 import { provideRouter, withRouterConfig } from '@angular/router';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
