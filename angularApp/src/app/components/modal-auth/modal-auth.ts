99853824ebcd4c60e9397e29331747f03f9e6339
Talia Samantha <taliasamanthar@gmail.com>
2025-11-16 12:40:02 +0100

    modal ok

---
+++ angularApp/src/app/components/modal-auth/modal-auth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-auth',
+  imports: [],
+  templateUrl: './modal-auth.html',
+  styleUrl: './modal-auth.css',
+})
+export class ModalAuth {
+
+}
--- angularApp/src/app/components/navbar/navbar_component.ts
+++ angularApp/src/app/components/navbar/navbar_component.ts
@@ -2,18 +2,36 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SearchService } from '../../services/searchService';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'navbar-page',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,FormsModule],
   templateUrl: './navbar_component.html',
   styleUrls: ['./navbar_component.css'],
 })
 export class NavbarPageComponent {
+  modalIsOpen: boolean = false;
+  request = {
+  username: '',
+  email: '',
+  message: ''
+};
   constructor(private searchService: SearchService) {}
 
-onSearch(term: string) {
-  this.searchService.setSearchTerm(term);
+  onSearch(term: string) {
+    this.searchService.setSearchTerm(term);
+  }
+  isOpen():void {
+    this.modalIsOpen = true
+    console.log(this.modalIsOpen)
+  }
+  closeModal():void {
+    this.modalIsOpen = false
+  }
+  submitRequest() {
+  console.log('Request inviata:', this.request);
+  alert('Richiesta inviata con successo!');
+  this.closeModal();
+  this.request = { username: '', email: '', message: '' }; // reset
 }
 }
